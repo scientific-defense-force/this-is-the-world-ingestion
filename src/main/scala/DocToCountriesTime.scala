a00c66b0468c884cb5a76949e2d43e121adfaff1
Alistair Burrowes <alistair.burrowes@rea-group.com>
2016-12-12 19:44:06 +1100

    switch

--- src/main/scala/DocToCountriesTime.scala
+++ src/main/scala/DocToCountriesTime.scala
@@ -41,27 +41,37 @@ object DocToCountryDetails {
   def lineToCountryDetail(line: String) : CountryDetail = {
     val regex = new Regex(
       """
-        | $([\w\s.-])
-        | \w(Africa|Asia-Pacific|Europe|North America|Latin America)
-        | \w(Low income|Lower middle income|Upper middle income|High income)
-        | \w+([0-9,]+)
-        | \w+([0-9,]+)
-        | \w+([0-9,]+)
-        | \w+([0-9,]+)
-        | \w+([0-9.]+)\w
+        | ^([\w\s.-]+)
+        | \s(Africa|Asia-Pacific|Europe|North America|Latin America)
+        | \s(Low income|Lower middle income|Upper middle income|High income)
+        | \s+([0-9,]+)
+        | \s+([0-9,]+)
+        | \s+([0-9,]+)
+        | \s+([0-9,]+)
+        | \s+([0-9.]+)\s
       """, "country", "region", "incomeGroup", "gdpPerAdult2016", "wealthPerAdult2000", "weatlhPerAdult2016", "totalWeatlh", "shareOfWorldWealth2016")
 
-    val result = regex.findAllIn(line)
+    val result = regex.findAllMatchIn(line)
 
-    new CountryDetail(
-      name = result.group("country"),
-      region = result.group("region"),
-      incomeGroup = result.group("incomeGroup"),
-      gdpPerAdult2016 = result.group("gdpPerAdult2016").filter((char) => char != ',').toInt,
-      wealthPerAdult2000 = result.group("wealthPerAdult2000").filter((char) => char != ',').toInt,
-      weatlhPerAdult2016 = result.group("weatlhPerAdult2016").filter((char) => char != ',').toInt,
-      totalWeatlh = result.group("totalWeatlh").filter((char) => char != ',').toInt,
-      shareOfWorldWealth2016 = result.group("shareOfWorldWealth2016").filter((char) => char != ',').toDouble
-    )
+//    result.next()
+
+    val ooh = result.next()
+
+//    val omg = result.group("country")
+
+    print(ooh)
+
+    null
+
+//    CountryDetail(
+//      name = result.group("country"),
+//      region = result.group("region"),
+//      incomeGroup = result.group("incomeGroup"),
+//      gdpPerAdult2016 = result.group("gdpPerAdult2016").filter((char) => char != ',').toInt,
+//      wealthPerAdult2000 = result.group("wealthPerAdult2000").filter((char) => char != ',').toInt,
+//      weatlhPerAdult2016 = result.group("weatlhPerAdult2016").filter((char) => char != ',').toInt,
+//      totalWeatlh = result.group("totalWeatlh").filter((char) => char != ',').toInt,
+//      shareOfWorldWealth2016 = result.group("shareOfWorldWealth2016").filter((char) => char != ',').toDouble
+//    )
   }
 }
