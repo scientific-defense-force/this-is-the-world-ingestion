fa58653f09944c9dbbbbe49146eaf978855891d9
Alistair Burrowes <alistair.burrowes@gmail.com>
2017-01-14 22:28:34 +1100

    Trying out wenao

---
+++ src/main/scala/util/CountryIsWenao.scala
@@ -0,0 +1,9 @@
+package util
+
+object CountryIsWenao {
+  val oecdCountries = List("Australia", "Austria", "Belgium", "Canada", "Denmark", "Finland", "France", "Germany", "Greece", "Iceland", "Ireland", "Italy", "Japan", "Luxembourg", "Netherlands", "New Zealand", "Norway", "Portugal", "Spain", "Sweden", "Switzerland", "Turkey", "United Kingdom", "United States")
+
+  def isIt(countryName: String) : Boolean = {
+      oecdCountries.contains(countryName)
+  }
+}
